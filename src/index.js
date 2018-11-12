e8f5e8b4531c464e82eb700a186a0d9392e65227
swapsCAPS <verstegen.daan@gmail.com>
2018-11-12 23:32:43 +0100

    omg drunken cube!

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
-console.log('hai!')
+import _ from 'underscore'
+import {
+  BoxGeometry,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
+} from 'three'
+
+import './stylesheet.less'
+
+const scene  = new Scene()
+const camera = new PerspectiveCamera(
+  75, window.innerWidth / window.innerHeight, 0.1, 1000
+)
+
+const renderer = new WebGLRenderer()
+
+renderer.setSize( window.innerWidth, window.innerHeight )
+
+document.body.appendChild( renderer.domElement )
+
+const geometry = new BoxGeometry( 1, 1, 1 )
+const material = new MeshBasicMaterial( { color: "tomato" } )
+
+const cube     = new Mesh( geometry, material )
+
+scene.add( cube )
+
+camera.position.z = 5
+
+// Start rendering, it doesnt do anyting otherwise : )
+
+function animate() {
+  requestAnimationFrame( animate )
+
+  // Animatiawns!
+
+  cube.rotation.x += (_.random(0, 10) * 0.01)
+  cube.rotation.y += (_.random(0, 20) * 0.01)
+
+
+  renderer.render( scene, camera )
+}
+animate()
+
