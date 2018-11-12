967023b60452857c3285d76b5494e9d2c21ad319
swapsCAPS <verstegen.daan@gmail.com>
2018-11-12 23:32:32 +0100

    webpack loaders 'n such

--- webpack.common.js
+++ webpack.common.js
@@ -18,8 +18,15 @@ module.exports = {
   ],
 
   module: {
-    rules: [
-
-    ]
+    rules: [{
+      test: /\.less$/,
+      use: [{
+        loader: 'style-loader' // creates style nodes from JS strings
+      }, {
+        loader: 'css-loader' // translates CSS into CommonJS
+      }, {
+        loader: 'less-loader' // compiles Less to CSS
+      }]
+    }]
   }
 }
