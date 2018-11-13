23fbc55b1cba4855a4d52fa6fef6212522952c21
swapsCAPS <verstegen.daan@gmail.com>
2018-11-13 23:04:21 +0100

    what we do in the shadows!

--- src/index.js
+++ src/index.js
@@ -1,69 +1,73 @@
 import _ from 'underscore'
 import {
   BoxGeometry,
+  BasicShadowMap,
+  PCFSoftShadowMap,
   Mesh,
   MeshBasicMaterial,
   MeshStandardMaterial,
+  MeshLambertMaterial,
   PerspectiveCamera,
   Scene,
   WebGLRenderer,
   AmbientLight,
+  PointLight,
   CubeTextureLoader,
   TextureLoader,
   RepeatWrapping,
 } from 'three'
+import * as THREE from 'three'
 
 import './stylesheet.less'
 
 import * as envMapImg from './assets/env-map.jpg'
 
+const ASPECT_RATIO = window.innerWidth / window.innerHeight
+const NEAR = 0.1
+const FAR  = 10000
+
 const scene  = new Scene()
-const camera = new PerspectiveCamera(
-  75, window.innerWidth / window.innerHeight, 0.1, 1000
-)
+const camera = new PerspectiveCamera(75, ASPECT_RATIO, NEAR, FAR)
 
-const renderer = new WebGLRenderer()
+const renderer = new WebGLRenderer( { antialias: false } )
 
+renderer.setClearColor(0x4f4f4f)
 renderer.setSize( window.innerWidth, window.innerHeight )
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type    = PCFSoftShadowMap
 
 document.body.appendChild( renderer.domElement )
 
-const geometry = new BoxGeometry( 1, 1, 1 )
+const floor = new Mesh(
+  new THREE.PlaneGeometry( 600, 600, 600, 600 ),
+  new MeshLambertMaterial( { color: 0xfcfcfc, side: THREE.DoubleSide } )
+)
+floor.position.set( 0, 0, 0 )
+floor.receiveShadow = true
+floor.rotation.x -= Math.PI / 2
 
-const envMapLoader = new CubeTextureLoader()
-const textureCube = envMapLoader.load([
-  envMapImg,
-  envMapImg,
-  envMapImg,
-  envMapImg,
-  envMapImg,
-  envMapImg,
-])
+const cube  = new Mesh(
+  new BoxGeometry( 50, 50, 50 ),
+  new MeshLambertMaterial( { color: 0x555555 } )
+)
+cube.position.set(20, 60, 20)
+cube.receiveShadow = true
+cube.castShadow = true
 
-const material = new MeshStandardMaterial({
-  color: "",
-  opacity: 0.5,
-  roughness: 1.0,
-  metalness: 0.0,
-  emmisive: "tomato",
-  envMap: textureCube,
-})
+const light1 = new AmbientLight( 0xFFFFFF, 0.5 )
+light1.position.set( 150, 150, 150 )
+const light2 = new PointLight( 0x4FFFFFF, 0.5 )
+light2.position.set( 150, 200, 150 )
+light2.lookAt(cube.position)
+light2.castShadow = true
 
-const texture = new TextureLoader().load(envMapImg)
-texture.wrapS = RepeatWrapping
-texture.wrapT = RepeatWrapping
-
-const newMaterial = new MeshBasicMaterial({
-  map: texture
-})
-
-const light = new AmbientLight( 0x404040 ); // soft white light
-const cube     = new Mesh( geometry, newMaterial )
-
+scene.add( light1 )
+scene.add( light2 )
+scene.add( floor )
 scene.add( cube )
-scene.add( light );
 
-camera.position.z = 5
+camera.position.set(150, 250, 200)
+camera.lookAt(cube.position)
 
 // Start rendering, it doesnt do anyting otherwise : )
 
@@ -72,8 +76,8 @@ function animate() {
 
   // Animatiawns!
 
-  cube.rotation.x += (_.random(1, 5) * 0.00125) // random() === cube.drunk
-  cube.rotation.y += (_.random(1, 5) * 0.00125)
+  cube.rotation.x += 0.01 // (_.random(1, 5) * 0.00125) // random() === cube.drunk
+  cube.rotation.y += 0.01 // (_.random(1, 5) * 0.00125)
 
 
   renderer.render( scene, camera )
